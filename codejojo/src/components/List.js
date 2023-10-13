3d1d28351470f1e791d4e0cab71e8445b94e4ec6
JojoPrc <joseproenca2005@gmail.com>
2023-10-12 21:47:38 -0300

    cria pasta components

---
+++ codejojo/src/components/List.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import Styles from './list.module.css';
+import Button from '../button/Button';
+import Input from '../Input/Input';
+
+// Resto do código do componente List
+
+export default List;
